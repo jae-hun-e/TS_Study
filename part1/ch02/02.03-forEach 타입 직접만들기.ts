ff80eb5b668cf6a198275e2656158dd6c2ecfdef
hun2__2 <wognskec@hanyang.ac.kr>
2023-07-31 22:34:53 +0900

    forEach 타입 직접 만들기

---
+++ part1/ch02/02.03-forEach 타입 직접만들기.ts
@@ -0,0 +1,15 @@
+interface Arr<T>{
+    forEach(callbackfn : (item : T) => void): void
+//     정답
+//     forEach(callbackfn: (value: number, index: number, array: Int8Array) => void, thisArg?: any): void;
+}
+
+const myForEach1 : Arr<number> = [1,2,3]
+
+myForEach1.forEach((item) => console.log(item))
+
+const myForEach2 : Arr<string> = ["1","2","3"]
+myForEach2.forEach((item) => console.log(item))
+
+const myForEach3 : Arr<string | number> = ["1","2",3]
+myForEach2.forEach((item) => console.log(item))
